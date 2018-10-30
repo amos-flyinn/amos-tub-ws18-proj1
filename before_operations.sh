fd335fcda122c6efb1e31c801153d26e2b36f210
Alejandro Jaramillo <jaramilloalejo96@gmail.com>
2018-10-30 19:58:45 +0100

    Adding TravisCI to build the app in every commit or PR
    Co-authored-by: Sebastian Brito <s.britocorral@gmail.com>

---
+++ before_operations.sh
@@ -0,0 +1,8 @@
+#Giving permissions to travis for gradle
+chmod +x gradlew
+
+#Executing emulator for testing
+echo no | android create avd --force -n test -t android-$ANDROID_API_LEVEL_EMULATOR --abi default/armeabi-v7a
+emulator -avd test -no-window &
+android-wait-for-emulator
+adb shell input keyevent 82 &
