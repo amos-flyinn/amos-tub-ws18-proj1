5060f2eaf4a85426a371b1f4a4da5887d6e583ec
Alcasa <alcasa.mz@gmail.com>
2018-10-31 21:38:41 +0000

    Exploratory work on input event manipulation (#8)
    * Exploratory work on input event manipulation
    
    Simple processing of input touch and key events in a view widget with output
    to a TextView using a simple string msg output.
    
    * Demo in separate activity and input spoofing in lower split
    
    * Remove deprecated components

--- server/src/main/java/com/amos/server/MainActivity.java
+++ server/src/main/java/com/amos/server/MainActivity.java
@@ -3,16 +3,32 @@ package com.amos.server;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.content.Intent;
+
+import org.w3c.dom.Text;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    public void activateEventGrabDemo(View v) {
+        Intent intent = new Intent(MainActivity.this, EventGrabDemo.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
     @Override
