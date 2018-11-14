ba3a83ee6d563158577dd5ece558ae52e55b2863
Max Zhao <alcasa.mz@gmail.com>
2018-11-14 21:38:24 +0100

    Adding sender changes

--- server/src/main/java/com/amos/server/EventSenderDemo.java
+++ server/src/main/java/com/amos/server/EventSenderDemo.java
@@ -1,26 +1,25 @@
 package com.amos.server;
 
-import android.os.HandlerThread;
-import android.os.Looper;
-import android.os.Message;
+import com.amos.server.wifimanager.WifiServiceManager;
+import com.amos.server.eventsender.EventServer;
+
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.os.Handler;
-import android.util.EventLog;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.util.Log;
+import android.os.Build;
+import android.widget.Toast;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.p2p.WifiP2pManager;
 
-import com.amos.server.eventsender.EventServer;
-
-import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+
 
 public class EventSenderDemo extends AppCompatActivity {
 
@@ -28,12 +27,48 @@ public class EventSenderDemo extends AppCompatActivity {
 
     BlockingQueue<MotionEvent> mq;
 
+    private final IntentFilter intentFilter = new IntentFilter();
+    private WifiP2pManager.Channel mChannel;
+    private WifiP2pManager mManager;
+    private WifiServiceManager receiver;
+    private static final int COARSE_LOCATION = 1001;
+
+    protected void createP2P(Context context) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this,"Requesting permission for peers",Toast.LENGTH_SHORT).show();
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, COARSE_LOCATION);
+        }
+        // Indicates a change in the Wi-Fi P2P status.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        // Indicates a change in the list of available peers.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        // Indicates the state of Wi-Fi P2P connectivity has changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        // Indicates this device's details have changed.
+        intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        this.mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        this.mChannel = mManager.initialize(this, getMainLooper(), null);
+        //Making the smartphone in discovery mode for other peers
+        this.mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Toast.makeText(context, "Listening to Peers", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFailure(int i) {
+                Toast.makeText(context,"Error listening to Peers", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_sender_demo);
         base = findViewById(R.id.senderlayout);
         mq = new LinkedBlockingQueue<MotionEvent>();
+        createP2P(this);
     }
 
     @Override
@@ -44,7 +79,8 @@ public class EventSenderDemo extends AppCompatActivity {
                 new OnTouchListener() {
                     @Override
                     public boolean onTouch(View v, MotionEvent event) {
-                        // Log.d("Test", event.toString());
+                        event.setLocation(event.getX() / base.getWidth(), event.getY() / base.getHeight());
+                        Log.d("Test", event.toString());
                         mq.add(event);
                         return true;
                     }
