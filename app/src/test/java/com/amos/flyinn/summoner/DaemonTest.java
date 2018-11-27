af44a40d33540af72619d85f317aafd4f43d9891
Max Zhao <alcasa.mz@gmail.com>
2018-11-27 23:02:03 +0100

    Letting initial daemontest use roboelectric for running tests

--- app/src/test/java/com/amos/flyinn/summoner/DaemonTest.java
+++ app/src/test/java/com/amos/flyinn/summoner/DaemonTest.java
@@ -3,14 +3,18 @@ package com.amos.flyinn.summoner;
 import android.content.Context;
 import android.graphics.Point;
 
-import org.junit.Test;
 
 import androidx.test.core.app.ApplicationProvider;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+
+@RunWith(RobolectricTestRunner.class)
 public class DaemonTest {
 
     private Context context = ApplicationProvider.getApplicationContext();
-
     @Test
     public void spawn_adb() {
         Daemon d = new Daemon(context, "127.0.0.1", new Point());
