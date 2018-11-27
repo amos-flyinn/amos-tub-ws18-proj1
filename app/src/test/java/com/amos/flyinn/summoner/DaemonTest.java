25122ad6adca25887d0d0f745cc0d9bd20cd4a43
León Avilés <lv.avilesp@yahoo.de>
2018-11-27 23:40:29 +0100

    context tests for spawn_adb
    Co-authored-by: Eliza Danila <danilaeliza@gmail.com>

--- app/src/test/java/com/amos/flyinn/summoner/DaemonTest.java
+++ app/src/test/java/com/amos/flyinn/summoner/DaemonTest.java
@@ -7,6 +7,7 @@ import android.graphics.Point;
 import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
@@ -15,8 +16,28 @@ import org.robolectric.RobolectricTestRunner;
 public class DaemonTest {
 
     private Context context = ApplicationProvider.getApplicationContext();
+
     @Test
-    public void spawn_adb() {
-        Daemon d = new Daemon(context, "127.0.0.1", new Point());
+    public void spawn_adbHappypathTest() {
+        Daemon d = new Daemon(context, "127.0.0.1", new Point(0, 0));
+        boolean exception = false;
+        try {
+            d.spawn_adb();
+        } catch (Exception e) {
+            exception = true;
+        }
+        Assert.assertFalse(exception);
+    }
+
+    @Test
+    public void spawn_adbNoContextTest() {
+        Daemon d = new Daemon(null, "127.0.0.1", new Point(0, 0));
+        boolean exception = false;
+        try {
+            d.spawn_adb();
+        } catch (Exception e) {
+            exception = true;
+        }
+        Assert.assertTrue(exception);
     }
 }
