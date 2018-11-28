ddc775c493b4978a29b50f0289b92beb66707a38
Max Zhao <alcasa.mz@gmail.com>
2018-11-28 02:03:28 +0100

    Adding documentation and another constuctor to create touchevents directly from motionevents

--- shared/src/main/java/com/amos/shared/TouchEvent.java
+++ shared/src/main/java/com/amos/shared/TouchEvent.java
@@ -5,6 +5,13 @@ import android.view.MotionEvent;
 
 import java.io.Serializable;
 
+/**
+ * Serializable version of TouchEvents which contain enough information to
+ * transfer touch inputs between different devices.
+ *
+ * Only MotionEvent information relevant to reconstructing usable MotionEvents are sent.
+ * X and Y are expected to be normalized to the range between 0 and 1.
+ */
 public class TouchEvent implements Serializable {
     public float x;
     public float y;
@@ -12,6 +19,13 @@ public class TouchEvent implements Serializable {
     public long downTime;
     private float max = 1;
 
+    /**
+     * Create Single Touchpoint event.
+     * @param x
+     * @param y
+     * @param action
+     * @param downTime
+     */
     public TouchEvent(float x, float y, int action, long downTime) {
         this.x = x;
         this.y = y;
@@ -19,10 +33,31 @@ public class TouchEvent implements Serializable {
         this.downTime = downTime;
     }
 
+    /**
+     * Create Single Touchpoint event directly from MotionEvent.
+     * @param m
+     */
+    public TouchEvent(MotionEvent m) {
+        this.x = m.getX();
+        this.y = m.getY();
+        this.action = m.getAction();
+        this.downTime = m.getDownTime();
+    }
+
+    /**
+     * Create MotionEvent from given TouchEvent.
+     * @param maxX
+     * @param maxY
+     * @return
+     */
     public MotionEvent getConstructedMotionEvent(int maxX, int maxY) {
         return MotionEvent.obtain(downTime, SystemClock.uptimeMillis(), action, (x / max) * maxX, (y / max) * maxY, 0);
     }
 
+    /**
+     * Create String representation of TouchEvent.
+     * @return
+     */
     @Override
     public String toString() {
         return String.format("TE: X: %f Y: %f", x, y);
