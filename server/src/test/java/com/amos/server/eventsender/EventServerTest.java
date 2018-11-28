177a629609644bf442e1c594540bf4bad052680b
Max Zhao <alcasa.mz@gmail.com>
2018-11-28 01:35:37 +0100

    Initial test scaffolds

---
+++ server/src/test/java/com/amos/server/eventsender/EventServerTest.java
@@ -0,0 +1,29 @@
+package com.amos.server.eventsender;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventServerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void accept() {
+    }
+
+    @Test
+    public void close() {
+    }
+
+    @Test
+    public void acceptQueue() {
+    }
+
+    @Test
+    public void run() {
+    }
+}
---
+++ server/src/test/java/com/amos/server/eventsender/EventWriterTest.java
@@ -0,0 +1,25 @@
+package com.amos.server.eventsender;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventWriterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void write() {
+    }
+
+    @Test
+    public void write1() {
+    }
+
+    @Test
+    public void close() {
+    }
+}
