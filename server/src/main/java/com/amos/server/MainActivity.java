e4d78ec77f965919488e74e6ff545ae8d95c7a31
León Avilés <lv.avilesp@yahoo.de>
2018-12-03 23:28:44 +0100

    finished nearby implementation for server
    Co-authored-by: Eliza Danila <danilaeliza@gmail.com>

--- server/src/main/java/com/amos/server/MainActivity.java
+++ server/src/main/java/com/amos/server/MainActivity.java
@@ -1,10 +1,19 @@
 package com.amos.server;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,6 +30,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.amos.server.eventsender.EventServer;
 import com.amos.server.signaling.Emitter;
@@ -28,7 +38,18 @@ import com.amos.server.signaling.WebServer;
 import com.amos.server.webrtc.IPeer;
 import com.amos.server.webrtc.PeerWrapper;
 import com.amos.shared.TouchEvent;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.connection.AdvertisingOptions;
+import com.google.android.gms.nearby.connection.ConnectionInfo;
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
+import com.google.android.gms.nearby.connection.ConnectionResolution;
+import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.Payload;
+import com.google.android.gms.nearby.connection.PayloadCallback;
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
+import com.google.android.gms.nearby.connection.Strategy;
 
+import java.security.SecureRandom;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
@@ -47,6 +68,30 @@ public class MainActivity extends Activity {
     private PeerWrapper peerWrapper;
     private Button buttonInit;
     private SurfaceViewRenderer remoteRender;
+
+    /** Permissions required for Nearby Connection */
+    private static final String[] REQUIRED_PERMISSIONS =
+            new String[] {
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_WIFI_STATE,
+                    Manifest.permission.CHANGE_WIFI_STATE,
+            };
+
+    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
+
+    /** 1-to-1 since a device will be connected to only one other device at most. */
+    private static final Strategy STRATEGY = Strategy.P2P_POINT_TO_POINT;
+
+    /** Connection manager for the connection to FlyInn clients.*/
+    protected ConnectionsClient connectionsClient;
+
+    private final String serverName = generateName();
+    private String clientID;
+    private String clientName;
+
+    /** Tag for logging purposes. */
+    private static final String NEARBY_TAG = "ServerNearbyConnection";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -90,6 +135,9 @@ public class MainActivity extends Activity {
         this.peerWrapper.setEmitter((Emitter)this.webSocketServer);
         this.webSocketServer.start();
 
+        // initiate nearby connection manager
+        connectionsClient = Nearby.getConnectionsClient(this);
+
         senderRunner = new Thread(eventSender);
         senderRunner.start();
         view.setOnTouchListener(
@@ -110,10 +158,20 @@ public class MainActivity extends Activity {
         remoteRender = findViewById(R.id.surface_remote_viewer);
     }
 
+    /**
+     * Checks whether the app has the required permissions to establish connections,
+     * and then starts advertising to search for clients.
+     */
     @Override
     protected void onStart() {
         super.onStart();
         connectionInfo.setText("Waiting for P2P Wifi connection");
+
+        if (!hasPermissions(this, REQUIRED_PERMISSIONS)) {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_REQUIRED_PERMISSIONS);
+        }
+
+        startAdvertising();
     }
 
     @Override
@@ -128,8 +186,10 @@ public class MainActivity extends Activity {
         Intent intent;
         switch (item.getItemId()) {
             case R.id.p2pserver_activity:
-                intent = new Intent(this,P2PActivityServer.class);
-                break;
+                // skip since we use nearby
+                // intent = new Intent(this,P2PActivityServer.class);
+                // break;
+                return super.onOptionsItemSelected(item);
             case R.id.webrtc_server_activity:
                 intent = new Intent(this,WebRTCServerActivity.class);
                 break;
@@ -150,6 +210,172 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * Stops all nearby advertising and connections from this server before calling super.onStop().
+     */
+    @Override
+    protected void onStop() {
+        connectionsClient.stopAdvertising();
+        connectionsClient.stopAllEndpoints();
+        super.onStop();
+    }
 
+    /**
+     * Broadcast our presence using Nearby Connection so FlyInn users can find us.
+     * Resets clientID and clientName first.
+     */
+    private void startAdvertising() {
+        clientID = null;
+        clientName = null;
 
+        AdvertisingOptions advertisingOptions =
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build();
+        connectionsClient.startAdvertising(serverName, "com.amos.flyinn",
+                connectionLifecycleCallback, advertisingOptions)
+                .addOnSuccessListener(
+                        (Void unused) -> {
+                            // started advertising successfully
+                            Log.i(NEARBY_TAG, "Started advertising " + serverName);
+                            Toast.makeText(this, R.string.success_advertising,
+                                    Toast.LENGTH_SHORT).show();
+                        })
+                .addOnFailureListener(
+                        (Exception e) -> {
+                            // unable to advertise
+                            Log.e(NEARBY_TAG, "Unable to start advertising " + serverName);
+                            Toast.makeText(this, R.string.error_advertising,
+                                    Toast.LENGTH_LONG).show();
+                            finish();
+                        });
+    }
+
+    /**
+     * Obtain data from clientID/clientName via this handle.
+     */
+    private final PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    // TODO
+                }
+
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                    // TODO
+                }
+            };
+
+    /**
+     * Callbacks for connections to other devices.
+     * Includes token authentication and connection handling.
+     */
+    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+            new ConnectionLifecycleCallback() {
+                @Override
+                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
+                    Log.i(NEARBY_TAG, "Connection initiated by " + endpointId);
+                    clientName = connectionInfo.getEndpointName();
+
+                    // authentication via tokens
+                    // TODO replace token authentication with QR code/manual code input
+                    new AlertDialog.Builder(getApplicationContext())
+                            .setTitle("Accept connection to " + connectionInfo.getEndpointName())
+                            .setMessage("Confirm the code matches on both devices: " +
+                                    connectionInfo.getAuthenticationToken())
+                            .setPositiveButton("Accept",
+                                    (DialogInterface dialog, int which) ->
+                                            // accept the connection
+                                            connectionsClient.acceptConnection(endpointId,
+                                                    payloadCallback))
+                            .setNegativeButton(
+                                    android.R.string.cancel,
+                                    (DialogInterface dialog, int which) ->
+                                            // reject the connection
+                                            connectionsClient.rejectConnection(endpointId))
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+
+                @Override
+                public void onConnectionResult(String endpointId, ConnectionResolution result) {
+                    if (result.getStatus().isSuccess()) {
+                        Log.i(NEARBY_TAG, "Connected with " + endpointId);
+
+                        connectionsClient.stopAdvertising();
+
+                        clientID = endpointId;
+                    } else {
+                        Log.i(NEARBY_TAG, "Failure connecting with " + endpointId);
+                        clientName = null;
+                        clientID = null;
+                    }
+                }
+
+                @Override
+                public void onDisconnected(String endpointId) {
+                    Log.i(NEARBY_TAG, "Disconnected from " + endpointId);
+                    startAdvertising();
+                }
+            };
+
+    /**
+     * Determines whether the FlyInn server app has the necessary permissions to run nearby.
+     * @param context Checks the permissions against this context/application environment
+     * @param permissions The permissions to be checked
+     * @return True if the app was granted all the permissions, false otherwise
+     */
+    private static boolean hasPermissions(Context context, String[] permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Handles user acceptance (or denial) of our permission request.
+     * @param requestCode The request code passed in requestPermissions()
+     * @param permissions Permissions that must be granted to run nearby connections
+     * @param grantResults Results of granting permissions
+     */
+    @CallSuper
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode != REQUEST_CODE_REQUIRED_PERMISSIONS) {
+            return;
+        }
+
+        for (int grantResult : grantResults) {
+            if (grantResult == PackageManager.PERMISSION_DENIED) {
+                Log.w(NEARBY_TAG, "Permissions necessary for " +
+                        "Nearby Connection were not granted.");
+                Toast.makeText(this, R.string.error_missing_permissions,
+                        Toast.LENGTH_LONG).show();
+                finish();
+                return;
+            }
+        }
+        recreate();
+    }
+
+    /**
+     * Generates a name for the server.
+     * @return The server name, consisting of a random string appended to the build model
+     */
+    // TODO Define better name system?
+    private String generateName(){
+        String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+
+        StringBuilder sb = new StringBuilder(5);
+        for (int i = 0; i < 5; i++) {
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        }
+        return Build.MODEL + "_" + sb.toString();
+    }
 }
