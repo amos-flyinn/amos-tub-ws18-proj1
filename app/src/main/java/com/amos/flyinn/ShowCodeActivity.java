880a26ea5f18095499f6f2c1cdd70cb31d78a0cb
Charlie Nguyen <schorlie@Charlies-MBP.local>
2019-01-23 23:41:17 +0100

    Codacy, delete not used imports
    Co-Authored-By: Leon Aviles <lv.avilesp@yahoo.de>
    Co-Authored-by: Eliza Danila <danilaeliza@gmail.com>

--- app/src/main/java/com/amos/flyinn/ShowCodeActivity.java
+++ app/src/main/java/com/amos/flyinn/ShowCodeActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.os.Build;
